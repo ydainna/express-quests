da5db11f28568788992076542ebfc4fd778cda2d
saigyou_ayakashi <minuit1993@live.fr>
2022-11-02 20:24:39 +0100

    First commit add project file

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.APP_PORT ?? 5000;
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
